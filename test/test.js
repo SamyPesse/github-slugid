6ca877ecbbffd3e6c91906083c2140fbe47129bb
Samy Pessé <samypesse@gmail.com>
2016-04-04 11:02:05 +0200

    Should not replace - by space

--- test/test.js
+++ test/test.js
@@ -1,31 +1,29 @@
 var slug = require('../');
 var should = require('should');
 
+var MATCHES = {
+    'hello': 'hello',
+    'hello world': 'hello-world',
+    '!weird + id/for headings': 'weird--idfor-heading',
+    '您好': '您好',
+    'I ♥ you': 'i--you',
+    'a > b': 'a--b',
+    'Schöner Titel läßt grüßen!? Bel été !': 'schöner-titel-läßt-grüßen-bel-été-'
+}
+
+
+
 describe('slug', function () {
-    it('should replace whitespaces', function() {
-        slug('hello').should.equal('hello');
-        slug('hello world').should.equal('hello-world');
-    });
+    for (var value in MATCHES) {
+        var expected = MATCHES[value];
 
-    it('should replace with lowercase', function() {
-        slug('Hello World').should.equal('hello-world');
-    });
+        it('should handle '+ JSON.stringify(value), function() {
+            slug(value).should.equal(expected);
+        });
 
-    it('should remove leading separator', function() {
-        slug('!weird + id/for headings').should.equal('weird--idfor-headings');
-    });
-
-    it('should accept chinese', function() {
-        slug('您好').should.equal('您好');
-    });
-
-    it('should remove symbols', function() {
-        slug('I ♥ you').should.equal('i--you');
-        slug('a > b').should.equal('a--b');
-    });
-
-    it('should follow github', function() {
-        slug('Schöner Titel läßt grüßen!? Bel été !').should.equal('schöner-titel-läßt-grüßen-bel-été-');
-    })
+        it('should handle '+ JSON.stringify(expected), function() {
+            slug(expected).should.equal(expected);
+        });
+    }
 });
 
